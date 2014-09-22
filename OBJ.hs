4f4042815c2ad4f1a1514a67168e46d951dbcb27
Neil Mitchell <ndmitchell@gmail.com>
2014-09-22 20:53:36 +0100

    Redo the OBJ library to give a more principled Vertex type

--- OBJ.hs
+++ OBJ.hs
@@ -1,23 +1,67 @@
 {-# LANGUAGE RecordWildCards #-}
 
 module OBJ(
-    Vertex(..), Vector,
-    Face(..), showOBJ,
-    normal, unit, follow
+    Vertex(..), size, unit, normal,
+    Face(..), showOBJ
     ) where
 
 import Numeric
 import qualified Data.Map as Map
 
 
-data Vertex = Vertex {x, y, z :: Double} deriving (Eq,Ord)
+---------------------------------------------------------------------
+-- VERTEX LIBRARY
 
-type Vector = Vertex
+data Vertex = Vertex {x, y, z :: Double} deriving (Show,Eq,Ord)
+
+instance Num Vertex where
+    fromInteger = lift0 . fromInteger
+    signum = lift0 . signum . size
+    abs = lift1 abs
+    negate = lift1 negate
+    (-) = lift2 (-)
+    (+) = lift2 (+)
+    (*) = lift2 (*)
+
+instance Fractional Vertex where
+    fromRational = lift0 . fromRational
+    (/) = lift2 (/)
+    recip = lift1 recip
+
+lift0 :: Double -> Vertex
+lift0 v = Vertex v v v
+
+lift1 :: (Double -> Double) -> Vertex -> Vertex
+lift1 f v = Vertex (g x) (g y) (g z)
+    where g sel = f $ sel v
+
+lift2 :: (Double -> Double -> Double) -> Vertex -> Vertex -> Vertex
+lift2 f v1 v2 = Vertex (g x) (g y) (g z)
+    where g sel = f (sel v1) (sel v2)
+
+normal :: Vertex -> Vertex -> Vertex -> Vertex
+normal (Vertex x1 y1 z1) (Vertex x2 y2 z2) (Vertex x3 y3 z3) = Vertex
+    ((y2-y1)*(z3-z1) - (y3-y1)*(z2-z1))
+    ((z2-z1)*(x3-x1) - (x2-x1)*(z3-z1))
+    ((x2-x1)*(y3-y1) - (x3-x1)*(y2-y1))
 
 
-data Face = Face {verticies :: [Vertex], normals :: [Vector]}
+size :: Vertex -> Double
+size (Vertex x y z) = sqrt (sqr x + sqr y + sqr z)
+    where sqr v = v * v
 
-data S = S {vs :: Map.Map Vertex Int, vns :: Map.Map Vector Int}
+unit :: Vertex -> Vertex
+unit v | n == 0 = v
+       | otherwise = v / fromRational (toRational n)
+    where n = size v
+
+
+---------------------------------------------------------------------
+-- OBJ LIBRARY
+
+data Face = Face {verticies :: [Vertex], normals :: [Vertex]}
+
+data S = S {vs :: Map.Map Vertex Int, vns :: Map.Map Vertex Int}
 
 showOBJ :: [Face] -> [String]
 showOBJ = showFaces $ S Map.empty Map.empty
@@ -49,23 +93,3 @@ showOBJ = showFaces $ S Map.empty Map.empty
 shw x = showFFloat Nothing x ""
 
 
-normal :: Vertex -> Vertex -> Vertex -> Vertex
-normal (Vertex x1 y1 z1) (Vertex x2 y2 z2) (Vertex x3 y3 z3) = Vertex
-    ((y2-y1)*(z3-z1) - (y3-y1)*(z2-z1))
-    ((z2-z1)*(x3-x1) - (x2-x1)*(z3-z1))
-    ((x2-x1)*(y3-y1) - (x3-x1)*(y2-y1))
-
-
-len :: Vertex -> Double
-len (Vertex x y z) = sqrt (sqr x + sqr y + sqr z)
-    where sqr v = v * v
-
-unit :: Vertex -> Vertex
-unit v@Vertex{..}
-    | n == 0 = v
-    | otherwise = Vertex (x/n) (y/n) (z/n)
-    where n = len v
-
-
-follow :: [Vertex] -> Vertex
-follow vs = Vertex (sum $ map x vs) (sum $ map y vs) (sum $ map z vs)
